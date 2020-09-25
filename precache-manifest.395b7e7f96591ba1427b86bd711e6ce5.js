c9c6c0f4ac37fad3edcc332a4c2c9d5b37baee61
pohan <brian780405@gmail.com>
2020-09-26 00:03:20 +0800

    no message

---
+++ precache-manifest.395b7e7f96591ba1427b86bd711e6ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74d36efa09f60f7bf245bd24dcb8b3b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e144d963fb39bcd987f",
+    "url": "./static/css/main.9680ec46.chunk.css"
+  },
+  {
+    "revision": "f9189bebf7296be719da",
+    "url": "./static/js/2.2f339c78.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.2f339c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e144d963fb39bcd987f",
+    "url": "./static/js/main.136d030a.chunk.js"
+  },
+  {
+    "revision": "9b659656a9be87e8a8d1",
+    "url": "./static/js/runtime-main.cc32eb63.js"
+  }
+]);
